b331628bf9207a6c40628a07ffee87fa3494d2b6
chmodx5 <chmodx5@gmail.com>
2022-02-06 14:28:07 +0300

    finalizing the footer

--- src/Components/Blocks/Layout/Footer.jsx
+++ src/Components/Blocks/Layout/Footer.jsx
@@ -7,18 +7,19 @@ import {
   FaRegEnvelope,
 } from "react-icons/fa";
 import { AiOutlineFieldTime } from "react-icons/ai";
-import { GrMapLocation } from "react-icons/gr";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import IconButton from "../../General/IconButton";
 import HeadingWithEffect from "../../General/HeadingWithEffect";
+import FooterContactListItem from "./FooterContactListItem";
 
 function Footer() {
   return (
     <footer>
       <div className="bg-gray-800 text-white">
-        <div className="container mx-auto py-16">
-          <div className="grid grid-cols-12 gap-5">
-            <div className="col-span-4">
+        <div className="container mx-auto py-16 px-6 sm:px-0">
+          <div className="grid grid-cols-12 gap-5 ">
+            <div className="col-span-12 md:col-span-4">
               <h1 className=" uppercase text-2xl font-bold mb-8">
                 <span className=" text-primary-default">nya</span>ngumi
               </h1>
@@ -36,38 +37,61 @@ function Footer() {
                 <IconButton icon={<FaGithubAlt />} />
               </div>
             </div>
-            <div className="col-span-3">
+            <div className="hidden md:block md:col-span-1"></div>
+
+            <div className="col-span-12 md:col-span-3">
               <HeadingWithEffect title="contact us" />
+              <br />
+              <br />
               <div>
-                <ul>
+                <ul className="footer-contacts-list">
                   <li>
-                    <GrMapLocation />
-                    <p>374 FA Tower, William S Blvd 2721, IL, USA</p>
+                    <FooterContactListItem icon={<HiOutlineLocationMarker />}>
+                      <p>374 FA Tower, William S Blvd 2721, IL, USA</p>
+                    </FooterContactListItem>
                   </li>
                   <li>
-                    <FaPhoneAlt />
-                    <p>(+880)155 69569 365</p>
+                    <FooterContactListItem icon={<FaPhoneAlt />}>
+                      <p>(+880)155 69569 365</p>
+                    </FooterContactListItem>
                   </li>
                   <li>
-                    <FaRegEnvelope />
-                    <p>support@rstheme.com</p>
+                    <FooterContactListItem icon={<FaRegEnvelope />}>
+                      <p>support@rstheme.com</p>
+                    </FooterContactListItem>
                   </li>
                   <li>
-                    <AiOutlineFieldTime />
-                    <p>Opening Hours: 10:00 - 18:00</p>
+                    <FooterContactListItem icon={<AiOutlineFieldTime />}>
+                      <p>Opening Hours: 10:00 - 18:00</p>
+                    </FooterContactListItem>
                   </li>
                 </ul>
               </div>
             </div>
-            <div className="col-span-5">
+            <div className="hidden md:block md:col-span-1"></div>
+
+            <div className="col-span-12 md:col-span-3 ">
               <HeadingWithEffect title="contact us" />
+              <br />
+              <br />
+
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Similique reiciendis asperiores
               </p>
-              <form action="">
-                <input type="email" name="" id="" />
-                <input type="submit" value="send" />
+              <form action="" className=" mt-12">
+                <div>
+                  <input
+                    type="email"
+                    className="py-3 text-black px-3 w-full"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <br />
+                <div>
+                  <input type="submit" className="btn" value="subscribe now" />
+                </div>
               </form>
             </div>
           </div>
@@ -75,16 +99,22 @@ function Footer() {
       </div>
       <div className="bg-gray-900 text-white py-8">
         <div className="container mx-auto">
-          <div className="grid grid-cols-12">
-            <div className="col-span-4">
+          <div className="grid grid-cols-12 gap-y-5">
+            <div className=" col-span-12 md:col-span-4 flex md:justify-end justify-center">
               © 2021 Dabble Inc. All Rights Reserved.
             </div>
-            <div className="col-span-8 flex justify-end">
-              <div className="flex gap-x-5">
+            <div className="col-span-12 md:col-span-8 flex md:justify-end justify-center">
+              <div className="flex divide-x divide-blue-200">
                 {/* to finish the styling of this links in the css file */}
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/" className="link">
+                  Home
+                </Link>
+                <Link to="/about" className="link">
+                  About
+                </Link>
+                <Link to="/contact" className="link">
+                  Contact
+                </Link>
               </div>
             </div>
           </div>
--- src/Components/General/Button.jsx
+++ src/Components/General/Button.jsx
@@ -3,11 +3,8 @@ import { Link } from "react-router-dom";
 
 function Button({ text, link }) {
   return (
-    <Link
-      to={link}
-      className="px-6 py-3 inline-block bg-primary-default hover:bg-primary-light"
-    >
-      <span className=" font-semibold uppercase text-white">{text}</span>
+    <Link to={link} className="btn">
+      <span className=" ">{text}</span>
     </Link>
   );
 }
