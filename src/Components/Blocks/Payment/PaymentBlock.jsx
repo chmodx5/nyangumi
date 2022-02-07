246d88c0e679864d56eba49a31d39bbc1f2b85a7
chmodx5 <chmodx5@gmail.com>
2022-02-07 08:05:30 +0300

    amde an array to loop thepaymentblock item and fixed all the styling

--- src/Components/Blocks/Payment/PaymentBlock.jsx
+++ src/Components/Blocks/Payment/PaymentBlock.jsx
@@ -1,12 +1,93 @@
 import React from "react";
 import PaymentBlockItem from "./PaymentBlockItem";
+import HeadingWithEffect from "./../../General/HeadingWithEffect";
 
 export default function PaymentBlock() {
+  const paymentBlockItems = [
+    {
+      package: "basic",
+      price: "11.19",
+      features: [
+        {
+          name: "Awesome Features",
+          available: true,
+        },
+        {
+          name: "Responsive pricing tables",
+          available: true,
+        },
+        {
+          name: "annula subscription",
+          available: false,
+        },
+        {
+          name: "Professional design",
+          available: false,
+        },
+      ],
+    },
+    {
+      package: "basic",
+      price: "11.19",
+      featured: true,
+      features: [
+        {
+          name: "Awesome Features",
+          available: true,
+        },
+        {
+          name: "Responsive pricing tables",
+          available: true,
+        },
+        {
+          name: "annula subscription",
+          available: true,
+        },
+        {
+          name: "Professional design",
+          available: false,
+        },
+      ],
+    },
+    {
+      package: "basic",
+      price: "11.19",
+      features: [
+        {
+          name: "Awesome Features",
+          available: true,
+        },
+        {
+          name: "Responsive pricing tables",
+          available: true,
+        },
+        {
+          name: "annula subscription",
+          available: true,
+        },
+        {
+          name: "Professional design",
+          available: true,
+        },
+      ],
+    },
+  ];
   return (
-    <div>
+    <div className="py-20">
       <div className="container mx-auto">
+        <HeadingWithEffect centered title="pricing plans" large />
+        <br />
+        <br />
         <div className="grid grid-cols-3 gap-6">
-          <PaymentBlockItem />
+          {paymentBlockItems.map((paymentBlockItem, index) => (
+            <PaymentBlockItem
+              key={index}
+              featured={paymentBlockItem.featured}
+              packagename={paymentBlockItem.package}
+              price={paymentBlockItem.price}
+              features={paymentBlockItem.features}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/Components/Blocks/Payment/PaymentBlockItem.jsx
+++ src/Components/Blocks/Payment/PaymentBlockItem.jsx
@@ -1,36 +1,36 @@
 import React from "react";
-import { TiTick } from "react-icons/ti";
+import { TiTick, TiTimes } from "react-icons/ti";
 import Button from "./../../General/Button";
 import "./styles.css";
 
-export default function PaymentBlockItem() {
+export default function PaymentBlockItem({
+  packagename,
+  price,
+  features,
+  featured,
+}) {
   return (
-    <div className="w-full bg-primary-default/5 p-10 flex flex-col items-center">
-      <h5 className="uppercase font-bold text-xl ">basic</h5>
+    <div
+      className={`w-full ${
+        featured ? "bg-gray-900 text-white" : "bg-primary-default/5"
+      }  p-10 flex flex-col items-center`}
+    >
+      <h5 className="uppercase font-bold text-xl ">{packagename}</h5>
       <br />
       <h1 className="font-bold text-primary-default text-7xl">
         <span className=" text-xl">$</span>
-        11.19
+        {price}
         <span className="text-xl">/mo</span>
       </h1>
 
       <ul className="mt-10 mb-16">
-        <li className="payment-features-list-item">
-          <TiTick />
-          <p>Awesome Features</p>
-        </li>
-        <li className="payment-features-list-item">
-          <TiTick />
-          <p>Responsive pricing table</p>
-        </li>
-        <li className="payment-features-list-item">
-          <TiTick />
-          <p>Yearly subscription</p>
-        </li>
-        <li className="payment-features-list-item">
-          <TiTick />
-          <p>Professional design</p>
-        </li>
+        {features.map((feature, index) => (
+          <li className="payment-features-list-item">
+            {feature.available ? <TiTick /> : <TiTimes />}
+
+            <p>{feature.name}</p>
+          </li>
+        ))}
       </ul>
       <Button link="/" text="subscribe" />
     </div>
