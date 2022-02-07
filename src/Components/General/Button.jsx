8887b85cb9417d86d50e7d8fb4e4c0994be9475f
chmodx5 <chmodx5@gmail.com>
2022-02-07 12:34:02 +0300

    adding the small attribute to the button component

--- src/Components/General/Button.jsx
+++ src/Components/General/Button.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Button({ text, link }) {
+function Button({ text, link, small }) {
   return (
-    <Link to={link} className="btn">
+    <Link
+      to={link}
+      className={` ${
+        small ? "py-1 px-3 text-md" : "px-6 py-3"
+      } inline-block bg-primary-default hover:bg-primary-light  font-semibold uppercase text-white`}
+    >
       <span className=" ">{text}</span>
     </Link>
   );
