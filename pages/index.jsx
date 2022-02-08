2fb51b59fbe7b053fec47703d44053217ed7b563
chmodx5 <chmodx5@gmail.com>
2022-02-08 08:31:26 +0300

    added tailwindcss to the project

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia quae
+        praesentium quia repellendus excepturi repellat quaerat officiis
+        officia, eum, exercitationem totam harum ducimus corporis voluptatem!
+        Suscipit nisi deleniti laboriosam quis.
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
