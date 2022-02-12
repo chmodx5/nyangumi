1ebeea609ea5a53eefaa1018bffdae67be2ba7eb
chmodx5 <chmodx5@gmail.com>
2022-02-12 19:07:13 +0300

    addimg a minimal product card

--- components/Blocks/Shop/Products/ProductCard.jsx
+++ components/Blocks/Shop/Products/ProductCard.jsx
@@ -4,7 +4,7 @@ import { FiShoppingCart } from "react-icons/fi";
 import Image from "next/image";
 import Rating from "./Rating";
 
-export default function ProductCard() {
+export default function ProductCard({ minimal }) {
   return (
     <div className="border border-gray-300 pb-2">
       <div className="bg-blue-300 relative w-full aspect-w-1 aspect-h-1">
@@ -17,31 +17,58 @@ export default function ProductCard() {
         />
       </div>
       <div className="px-2">
-        <small className="capitalize text-xs text-gray-500">snack</small>
+        {!minimal && (
+          <>
+            <small className="capitalize text-xs text-gray-500">snack</small>
+          </>
+        )}
         <h5 className="font-bold">
           All natural itallian style chicken meat balls
         </h5>
-        {/* <Rating /> */}
-        <p className="capitalize text-xs mt-2 mb-2 text-gray-500">
-          By <span className="text-primary-default">Someone</span>
-        </p>
-        <Rating rating={3.5} numReviews="3" />
-        <div className="flex justify-between mt-4  ">
-          <div className="flex gap-2  items-center ">
-            <p className="font-bold">
-              <span>$</span>
-              <span>10.00</span>
+
+        {minimal ? (
+          <>
+            <div className="flex justify-between mt-4  ">
+              <div className="flex gap-2  items-center ">
+                <p
+                  className={`font-bold${
+                    minimal ? " text-primary-default" : ""
+                  }`}
+                >
+                  <span>$</span>
+                  <span>10.00</span>
+                </p>
+                <p className="font-bold text-gray-400 text-sm line-through">
+                  <span>$</span>
+                  <span>15.00</span>
+                </p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="capitalize text-xs mt-2 mb-2 text-gray-500">
+              By <span className="text-primary-default">Someone</span>
             </p>
-            <p className="font-bold text-gray-400 text-sm line-through">
-              <span>$</span>
-              <span>15.00</span>
-            </p>
-          </div>
-          <button className="flex items-center gap-2 bg-primary-default/20  px-2 py-1 text-primary-default font-bold uppercase hover:bg-primary-default hover:text-white">
-            <FiShoppingCart />
-            add
-          </button>
-        </div>
+            <Rating rating={3.5} numReviews="3" />
+            <div className="flex justify-between mt-4  ">
+              <div className="flex gap-2  items-center ">
+                <p className="font-bold">
+                  <span>$</span>
+                  <span>10.00</span>
+                </p>
+                <p className="font-bold text-gray-400 text-sm line-through">
+                  <span>$</span>
+                  <span>15.00</span>
+                </p>
+              </div>
+              <button className="flex items-center gap-2 bg-primary-default/20  px-2 py-1 text-primary-default font-bold uppercase hover:bg-primary-default hover:text-white">
+                <FiShoppingCart />
+                add
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
