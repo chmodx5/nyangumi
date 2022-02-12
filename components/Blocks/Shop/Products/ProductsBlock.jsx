2d7fcd38f0e64b93411fb888eeb3591b923d8dcc
chmodx5 <chmodx5@gmail.com>
2022-02-12 19:21:47 +0300

    adding more items button

--- components/Blocks/Shop/Products/ProductsBlock.jsx
+++ components/Blocks/Shop/Products/ProductsBlock.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeadingWithEffect from "./../../../General/HeadingWithEffect";
 import ProductCard from "./ProductCard";
+import Link from "next/link";
 
 export default function ProductsBlock() {
   return (
@@ -8,9 +9,16 @@ export default function ProductsBlock() {
       <div className="mycontainer">
         <HeadingWithEffect large title="trending items" />
         <div className="grid grid-cols-5 mt-6 gap-5 ">
-          {Array.from({ length: 10 }, (_, i) => (
+          {Array.from({ length: 5 }, (_, i) => (
             <ProductCard minimal />
           ))}
+        </div>
+        <div className="mt-10">
+          <Link href="/">
+            <a className="bg-primary-default px-3 py-2 inline-block text-white font-bold uppercase hover:bg-primary-light">
+              more items
+            </a>
+          </Link>
         </div>
       </div>
     </section>
