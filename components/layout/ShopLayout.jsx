a834e81531ed062603d4c094da49cdbd23a0b758
chmodx5 <chmodx5@gmail.com>
2022-02-12 19:17:07 +0300

    cart button on shop navbar

--- components/layout/ShopLayout.jsx
+++ components/layout/ShopLayout.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "./../../components/Footer/Footer";
-
+import Link from "next/link";
+import { FiShoppingCart } from "react-icons/fi";
 export default function ShopLayout({ children }) {
   return (
     <div className="h-screen flex flex-col justify-between">
@@ -9,9 +10,17 @@ export default function ShopLayout({ children }) {
         <Navbar
           logo={
             <span>
-              {" "}
               <span className="text-primary-default">a-</span>shop
             </span>
+          }
+          navitems={
+            <>
+              <Link href="/">
+                <a className="hover:bg-primary-default p-3">
+                  <FiShoppingCart />
+                </a>
+              </Link>
+            </>
           }
         />
       </header>
