7fa45fc5113ba901b4ebd3e3eea43754d05a07da
chmodx5 <chmodx5@gmail.com>
2022-02-12 19:12:12 +0300

    logo in shop layout

--- components/layout/ShopLayout.jsx
+++ components/layout/ShopLayout.jsx
@@ -6,7 +6,14 @@ export default function ShopLayout({ children }) {
   return (
     <div className="h-screen flex flex-col justify-between">
       <header>
-        <Navbar />
+        <Navbar
+          logo={
+            <span>
+              {" "}
+              <span className="text-primary-default">a-</span>shop
+            </span>
+          }
+        />
       </header>
       <main className=" flex-1 mt-20">{children}</main>
       <Footer />
