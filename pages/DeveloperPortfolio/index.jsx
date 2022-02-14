6332b261161af54e2467aaef8b65c5cb200557af
chmodx5 <chmodx5@gmail.com>
2022-02-14 08:47:11 +0300

    adding my content toportfolio

---
+++ pages/DeveloperPortfolio/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import AboutBlockWithSliders from "../../components/Blocks/About/AboutBlockWithSliders";
+import ServicesBlock from "../../components/Blocks/Services/ServicesBlock";
+import PortfolioBlock from "../../components/Blocks/Portfolio/PortfolioBlock";
+import PricingBlock from "../../components/Blocks/Pricing/PricingBlock";
+import Hero from "../../components/Blocks/Hero/Hero";
+import BlogBlock from "../../components/Blocks/Blog/BlogBlock";
+import DeveloperPortfolioLayout from "../../components/layout/DeveloperPortfolioLayout";
+
+function Home() {
+  return (
+    <>
+      <Hero
+        smalltitle="Hello I&lsquo;m"
+        title="w. onesmas"
+        subtitle="a fullstack developer"
+        desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod deserunt illo asperiores tempora, dolor repellendus repellate  dolor repellendus repellate  dolor repellendus repellate "
+        buttonlink="/"
+        buttontext="discover more"
+      />
+      <ServicesBlock />
+      <AboutBlockWithSliders />
+      <PortfolioBlock />
+      <PricingBlock />
+      <BlogBlock />
+    </>
+  );
+}
+Home.getLayout = function getLayout(page) {
+  return <DeveloperPortfolioLayout>{page}</DeveloperPortfolioLayout>;
+};
+
+export default Home;
