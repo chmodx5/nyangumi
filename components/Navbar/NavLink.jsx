2c0728485f87e8fc6443fdfd75f092a31c24f0a2
chmodx5 <chmodx5@gmail.com>
2022-02-20 11:53:02 +0300

    setting the active link class in the navbar

--- components/Navbar/NavLink.jsx
+++ components/Navbar/NavLink.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import Router from "next/router";
 
 function NavLink({ link, text }) {
   return (
     <Link href={link} passHref>
-      <span className=" font-semibold uppercase px-4 text-sm  hover:text-primary-default hover:cursor-pointer">
+      <span
+        className={`font-semibold uppercase px-4 text-sm  hover:text-primary-default hover:cursor-pointer ${
+          Router.pathname == link ? " text-primary-default" : ""
+        }`}
+      >
         {text}
       </span>
     </Link>
